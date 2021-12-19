3d25e3604a3976b4f7d11da8a16260c7ff67ef23
Alieaes <u_mepia@naver.com>
2021-12-19 19:33:07 +0900

    라이브러리 폴더 예외

--- A2I/A2I.cpp
+++ A2I/A2I.cpp
@@ -1,8 +1,11 @@
 #include "stdafx.h"
 #include "A2I.h"
+#include "Logger/SharedLogger.h"
 
 A2I::A2I( QWidget* parent )
     : QMainWindow( parent )
 {
+    LOGD( "TEST", "VERBOSE TEST {} {}", "asd", "Asd" );
+
     ui.setupUi( this );
 }
